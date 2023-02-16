8cb146f4d96623293ed94c24925e57d64bd0d60b
merhan mostafa <merrymostafa14@gmail.com>
2023-02-16 12:10:42 +0200

    Upload files

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const pre = document.querySelector("pre");
+
+document.addEventListener("mousemove", (e) => {
+  rotateElement(e, pre);
+});
+
+function rotateElement(event, element) {
+  // get mouse position
+  const x = event.clientX;
+  const y = event.clientY;
+
+  // find the middle
+  const middleX = window.innerWidth / 2;
+  const middleY = window.innerHeight / 2;
+
+  // get offset from middle as a percentage
+  // and tone it down a little
+  const offsetX = ((x - middleX) / middleX) * 45;
+  const offsetY = ((y - middleY) / middleY) * 45;
+
+  // set rotation
+  element.style.setProperty("--rotateX", offsetX + "deg");
+  element.style.setProperty("--rotateY", -1 * offsetY + "deg");
+}
